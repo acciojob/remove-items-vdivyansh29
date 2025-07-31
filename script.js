36fe1ceb211d4572b8c9361fd1c43897d561e96c
Divyansh Verma <divyanshmotog5g@gmail.com>
2025-07-31 23:30:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+	const dropdown = document.getElementById("colorSelect")
+
+	function remove(){
+		const selectedIndex = dropdown.selectedIndex
+		if(selectedIndex >= 0){
+			dropdown.remove(selectedIndex)
+		}
+	}
