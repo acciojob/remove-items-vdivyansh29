706582c88bcb18112c7afa2d2a44771346145241
Divyansh Verma <divyanshmotog5g@gmail.com>
2025-08-01 14:47:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 	const dropdown = document.getElementById("colorSelect")
 
-	function remove(){
+	function removeList(){
 		const selectedIndex = dropdown.selectedIndex
 		if(selectedIndex >= 0){
 			dropdown.remove(selectedIndex)
